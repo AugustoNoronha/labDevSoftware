3cb93f528ff7bf6ebaca6d0fa55cb74cdf2c59c3
MiguelMagCastro <miguel.castro@sga.pucminas.br>
2024-09-03 01:26:31 -0300

    Refatoração dos metodos Adicionar aluno e Remover aluno da turma

--- implementacao/labDevSoftware/src/main/java/org/example/Models/Turmas.java
+++ implementacao/labDevSoftware/src/main/java/org/example/Models/Turmas.java
@@ -8,20 +8,28 @@ public class Turmas {
     public Disciplina disciplina;
     public List<Aluno> alunos;
 
-    public Turmas(Professor professor, Disciplina disciplina, List<Aluno> alunos) {
+    public Turmas(Professor professor, Disciplina disciplina) {
         this.professor = professor;
         this.disciplina = disciplina;
-        this.alunos = alunos;
+        this.alunos = new ArrayList<>();
     }
 
-    public List<Aluno> adicionarAluno(Aluno aluno){
-        List<Aluno> alunosList = new ArrayList<>();
-        return  alunosList;
+    public List<Aluno> adicionarAluno(Aluno aluno) {
+        if (this.alunos.size() < this.disciplina.getNUMERO_MAXIMO_ALUNOS()) {
+            this.alunos.add(aluno);
+        } else {
+            System.out.println("Não é possível adicionar aluno. Limite máximo de alunos atingido.");
+        }
+        return this.alunos;
     }
 
-    public List<Aluno> removerAluno(Aluno Aluno){
-        List<Aluno> alunosList = new ArrayList<>();
-        return  alunosList;
+    public List<Aluno> removerAluno(Aluno aluno) {
+        if (this.alunos.contains(aluno)) {
+            this.alunos.remove(aluno);
+        } else {
+            System.out.println("Aluno não encontrado na turma.");
+        }
+        return this.alunos;
     }
 
     public Professor selecionarProfessor(Professor professor){

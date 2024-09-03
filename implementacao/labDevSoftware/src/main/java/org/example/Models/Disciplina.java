a4f5096a695c8b47d2160c5cb8c3306b6be5e2d5
MiguelMagCastro <miguel.castro@sga.pucminas.br>
2024-09-03 01:15:22 -0300

    Adição Atributo boolean optativa.

--- implementacao/labDevSoftware/src/main/java/org/example/Models/Disciplina.java
+++ implementacao/labDevSoftware/src/main/java/org/example/Models/Disciplina.java
@@ -8,15 +8,16 @@ public class Disciplina {
     private int id;
     private String nome;
     private int CREDITOS;
+    private boolean optativa;
     private int CARGA_HORARIA;
-    private List<Disciplina> disciplina;
     private int NUMERO_MAXIMO_ALUNOS;
-    private org.example.Models.Enums.Status Status;
+    private Status Status;
 
-    public Disciplina(int id, String nome, int CREDITOS, int CARGA_HORARIA, List<Disciplina> disciplina, int NUMERO_MAXIMO_ALUNOS, org.example.Models.Enums.Status status) {
+    public Disciplina(int id, String nome, int CREDITOS, boolean optativa, int CARGA_HORARIA, List<Disciplina> disciplina, int NUMERO_MAXIMO_ALUNOS, org.example.Models.Enums.Status status) {
         this.id = id;
         this.nome = nome;
         this.CREDITOS = CREDITOS;
+        this.optativa = optativa;
         this.CARGA_HORARIA = CARGA_HORARIA;
         this.disciplina = disciplina;
         this.NUMERO_MAXIMO_ALUNOS = NUMERO_MAXIMO_ALUNOS;
@@ -39,12 +40,16 @@ public class Disciplina {
         return nome;
     }
 
-    public int getCARGA_HORARIA() {
-        return CARGA_HORARIA;
+    public int getCREDITOS() {
+        return this.CREDITOS;
     }
 
-    public List<Disciplina> getDisciplina() {
-        return disciplina;
+    public boolean isOptativa() {
+        return optativa;
+    }
+
+    public int getCARGA_HORARIA() {
+        return CARGA_HORARIA;
     }
 
     public int getNUMERO_MAXIMO_ALUNOS() {
@@ -54,5 +59,7 @@ public class Disciplina {
     public Status getStatus() {
         return Status;
     }
+
+
 
 }
